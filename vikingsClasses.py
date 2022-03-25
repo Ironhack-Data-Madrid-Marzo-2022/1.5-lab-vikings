cc6f274d432e9c395679e8580aafbe4c38388711
nombre_usuario_github <johndoe@example.com>
2022-03-25 18:16:26 +0100

    Respuestas

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,22 +3,79 @@
 
 
 class Soldier:
-    pass
-
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+        
+    def receiveDamage(self, damage):
+        self.health -= damage
+        
 # Viking
 
 
-class Viking:
-    pass
+class Viking (Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
+        
+        Soldier.__init__(self, health, strength)
+        
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
+        
+        if self.health > 0:
+            return self.name + ' has received ' + str(damage) + ' points of damage'
+        else:
+            return self.name + ' has died in act of combat'
+    
+    def battleCry(self):
+    	return 'Odin Owns You All!'
+        
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon (Soldier):
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+        
+        Soldier.__init__(self, health, strength)
+        
+    def attack(self):
+      return self.strength
+  
+    def receiveDamage(self, damage):
+        self.health -= damage
+        
+        if self.health > 0:
+            return 'A Saxon has received ' + str(damage) + ' points of damage'
+        else:
+            return 'A Saxon has died in combat'
+    
 
 # War
 
 
 class War:
-    pass
+    def __init__(self, vikingArmy, saxonArmy):
+        self.vikingArmy = []
+        self.saxonArmy = []
+        
+    def addViking(self, viking):
+        self.vikingArmy.append(viking)
+        
+    def addSaxon(self, viking):
+        self.saxonArmy.append(viking)
+        
+    def vikingAttack(self):
+        damage = viking.damage
+        
+        
---
+++ vikingsClasses2.py
@@ -0,0 +1,31 @@
+
+# Soldier
+
+
+class Soldier:
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        
+    def receiveDamage(self,damage):
+        self.damage = damage
+        
+# Viking
+
+
+class Viking:
+    def __init__
+
+# Saxon
+
+
+class Saxon:
+    pass
+
+# War
+
+
+class War:
+    pass
