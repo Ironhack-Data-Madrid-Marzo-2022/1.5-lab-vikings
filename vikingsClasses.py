8c51fd885cd50aefe50a60424155d885a76069be
trabalong <gastraba@gmail.com>
2022-03-25 16:47:21 +0100

    Segundo commit

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,3 +1,5 @@
+import random
+
 class Soldier:
 
     def __init__(self, health, strength):
@@ -20,25 +22,74 @@ class Viking(Soldier):
 
         self.name = name
         
-        self.health = health
-
-        self.strength = strength
+        Soldier.__init__(self, health, strength)
 
     def receiveDamage(self, damage = 0):
 
         self.health -= damage
 
-        if(self.health<=0): return f'{self.name} has died in act of combat'
+        if(self.health<=0): return self.name + ' has died in act of combat'
 
+        else: return self.name + ' has received ' + str(damage) + ' points of damage'
+    
+    def battleCry(self):
 
+        return 'Odin Owns You All!'
 
+class Saxon(Soldier):
 
+    def __init__(self, health = '', strength = ''):
 
+        Soldier.__init__(self, health, strength)
 
-class Saxon:
-    pass
+    def receiveDamage(self, damage=0):
 
+        self.health -= damage
 
+        if(self.health<=0): return 'A Saxon has died in combat'
 
+        else: return 'A Saxon has received ' + str(damage) + ' points of damage'
+    
 class War:
-    pass
+
+    def __init__(self):
+
+        self.vikingArmy = []
+
+        self.saxonArmy = []
+
+    def addViking(self, warrior):
+
+        self.vikingArmy.append(warrior)
+
+    def addSaxon(self, warrior):
+
+        self.saxonArmy.append(warrior)
+
+    def vikingAttack(self):
+
+        v = random.choice(self.vikingArmy)
+
+        s = random.choice(self.saxonArmy)
+
+        if(s.self.health==v.self.strength): self.saxonArmy.remove(s)
+
+        return s.receiveDamage(v.self.strength)
+
+    def saxonAttack(self):
+
+        v = random.choice(self.vikingArmy)
+
+        s = random.choice(self.saxonArmy)
+
+        if(v.self.health==s.self.strength): self.vikingArmy.remove(v)
+
+        return v.receiveDamage(s.self.strength)
+
+    def showStatus(self):
+
+        if(self.saxonArmy==[]): 
+
+
+
+    
