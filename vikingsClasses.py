5d41909b9bd6910dafdba187d47cb6f6fc9d6927
trabalong <gastraba@gmail.com>
2022-03-25 15:42:47 +0100

    Primer commit

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,23 +1,43 @@
-
-# Soldier
-
-
 class Soldier:
-    pass
 
-# Viking
+    def __init__(self, health, strength):
+
+        self.health = health
+        
+        self.strength = strength
+
+    def attack(self):
+        
+        return self.strength
+
+    def receiveDamage(self, damage = 0):
+
+        self.health -= damage
+
+class Viking(Soldier):
+
+    def __init__(self, name = '', health = '', strength = ''):
+
+        self.name = name
+        
+        self.health = health
+
+        self.strength = strength
+
+    def receiveDamage(self, damage = 0):
+
+        self.health -= damage
+
+        if(self.health<=0): return f'{self.name} has died in act of combat'
 
 
-class Viking:
-    pass
 
-# Saxon
+
 
 
 class Saxon:
     pass
 
-# War
 
 
 class War:
