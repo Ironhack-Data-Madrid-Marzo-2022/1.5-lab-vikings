84e4d8114e86a9e179efb93bc2da6641110b7730
Francisco Malo <fmalourarte@gmail.com>
2022-03-25 18:05:57 +0100

    aun no acabado

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,24 +1,106 @@
 
 # Soldier
-
-
+import random
 class Soldier:
-    pass
+
+    def __init__(self, health, strength):
+        self.health  = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self,damage):
+        self.health -= damage
 
 # Viking
 
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        self.health = health
+        self.strength = strength
 
-class Viking:
-    pass
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return(self.name+" has received "+str(damage)+" points of damage")
+        else: return(self.name+" has died in act of combat")
+
+    def battleCry(self):
+        return("Odin Owns You All!")
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+
+    def __init__(self, health, strength):
+        self.health=health
+        self.strength=strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return("A Saxon has received "+str(damage)+" points of damage")
+        else: return("A Saxon has died in combat")
+    
 
 # War
 
 
 class War:
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def addViking(self, viking):
+        self.vikingArmy.append(viking)
+
+    def addSaxon(self, saxon):
+        self.saxonArmy.append(saxon)
+
+    def vikingAttack(self):
+        idxSaxon = random.randint(0,9)
+        idxViking = random.randint(0,9)
+
+
+        unsaxon = self.saxonArmy[idxSaxon]
+        unviking= self.vikingArmy[idxViking]
+        result = unsaxon.receiveDamage(unviking.strength)
+
+        if unsaxon.health < 0:
+            self.saxonArmy.pop(idxSaxon)
+        
+        return (result)
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     pass
