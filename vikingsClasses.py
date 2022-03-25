fce922469e41788de2bd9e2e4553f4ec1e864ed6
lucasmigliano21 <lucasmigliano@gmail.com>
2022-03-25 16:59:11 +0100

    1

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -2,19 +2,52 @@
 # Soldier
 
 
+from unicodedata import name
+
+
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health=health
+        self.strength=strength
+    def attack(self):
+        return self.strength
+    def receiveDamage(self, damage):
+        self.health -= damage
+        pass
 
 # Viking
 
 
-class Viking:
+class Viking(Soldier):
+    def __init__(self,name,health,strength):
+        Soldier.__init__(self,health,strength)
+        self.name=name
+    def attack(self):
+        return self.strength
+    def receiveDamage(self,damage):
+        self.health -= damage
+        if self.health >0:
+            return self.name + " has received " + str(damage) + " points of damage"
+        else:
+            return self.name + " has died in act of combat"
+    def battleCry(self):
+            return "Odin Owns You All!"
     pass
 
 # Saxon
 
 
-class Saxon:
+class Saxon(Soldier):
+    def __init__(self,health,strength):
+        Soldier.__init__(self,health,strength)
+    def attack(self):
+        return self.strength
+    def receiveDamage(self,damage):
+        self.health -= damage
+        if self.health >0:
+            return "A Saxon has received " + str(damage) + " points of damage"
+        else:
+            return "A Saxon has died in combat"
     pass
 
 # War
