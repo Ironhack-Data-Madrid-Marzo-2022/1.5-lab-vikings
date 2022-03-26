8f49c2970b7f5df193065972b5c26581657e6eb1
Arturo-Rodriguez-Moreno <arturojrodriguezmoreno@gmail.com>
2022-03-26 14:38:18 +0100

    Class War

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -23,15 +23,17 @@ class Viking(Soldier):
     def __init__(self, name, health, strength):
         self.name = name
 
-        Soldier.__init__(self,health,strength)
+        Soldier.__init__(self, health, strength)
 
     def receiveDamage(self, damage):
         self.health -= damage
 
         if self.health > 0:
-            return self.name + " has received " + str(damage) + " points of damage"
+            info = self.name + " has received " + str(damage) + " points of damage"
+            return info
         else:
-            return self.name + " has died in act of combat"
+            info = self.name + " has died in act of combat"
+            return info
 
     def battleCry(self):
         return "Odin Owns You All!"
@@ -50,9 +52,11 @@ class Saxon(Soldier):
         self.health -= damage
 
         if self.health > 0:
-            return "A Saxon has received " + str(damage) + " points of damage"
+            info = "A Saxon has received " + str(damage) + " points of damage"
+            return info
         else:
-            return "A Saxon has died in combat"
+            info = "A Saxon has died in combat"
+            return info
 
 # War
 
@@ -70,23 +74,51 @@ class War:
         self.saxonArmy.append(saxon)
 
     def vikingAttack(self):
-        idex_saxon = random.randint(0, len(self.saxonArmy) - 1)
-        id_saxon = self.saxonArmy[idex_saxon]
 
-        idex_viking = random.randint(0, len(self.vikingArmy) - 1)
-        id_viking = self.vikingArmy[idex_viking]
+        index_saxon = random.randint(0, len(self.saxonArmy) - 1)
+        id_saxon = self.saxonArmy[index_saxon]
 
-        id_saxon.receiveDamage(id_viking.strength)
+        index_viking = random.randint(0, len(self.vikingArmy) - 1)
+        id_viking = self.vikingArmy[index_viking]
 
-
-
-
+        damage = id_viking.strength
+        life_saxon = id_saxon.receiveDamage(damage)
 
 
+        if id_saxon.health <= 0:
+            self.saxonArmy.pop(index_saxon)
+            return life_saxon
+        else:
+            return life_saxon
 
 
+    def saxonAttack(self):
+
+        index_saxon = random.randint(0, len(self.saxonArmy) - 1)
+        id_saxon = self.saxonArmy[index_saxon]
+
+        index_viking = random.randint(0, len(self.vikingArmy) - 1)
+        id_viking = self.vikingArmy[index_viking]
+
+        damage = id_saxon.strength
+        life_viking = id_viking.receiveDamage(damage)
 
 
+        if id_viking.health <= 0:
+            self.vikingArmy.pop(index_viking)
+            return life_viking
+        else:
+            return life_viking
+
+    def showStatus(self):
+
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+
+        elif len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
 
 
-        pass
