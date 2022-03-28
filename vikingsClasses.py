b971564ff5bba771f78897a5a1aae6c5cd196a25
Aplatm <aplatero.martin@gmail.com>
2022-03-28 16:05:37 +0200

    "Pull_v2"

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -2,20 +2,72 @@
 # Soldier
 
 
-class Soldier:
-    pass
+class Soldier():
+    def __init__(self, health, strength):
+
+            self.health = health
+
+            self.strength = strength
+
+    def attack(self):
+
+            return self.strength
+
+    def receiveDamage(self, damage):
+
+        self.health -= damage
+
 
 # Viking
 
+class Viking(Soldier): # de soldier
+    def __init__(self, name, health, strength):
 
-class Viking:
-    pass
+        self.name = name
+
+        self.health = health
+
+        self.strength = strength
+
+    def receiveDamage(self, damage):
+
+        self.health -= damage
+
+        if self.health <=0:
+
+            return f"{name} has died in act of combat"
+
+        else:
+            return f"{name}has received {damage} points of damage"
+
+    def battleCry(self):
+
+        return "Odin Owns You All!"
+
+
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier): # de soldier
+
+        def __init__(self, health, strength):
+
+            self.health = health
+
+            self.strength = strength
+
+        def receiveDamage(self, damage):
+
+            self.health -= damage
+
+            if self.health <= 0:
+
+                return " A saxon has died in combat"
+
+            else:
+
+                return f"A Saxon has received {damage} points of damage"
 
 # War
 
