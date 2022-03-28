3cf150a1de28c803847798155705080e3966e13f
nombre_usuario_github <johndoe@example.com>
2022-03-28 08:57:06 +0200

    lab 1.5 updated

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -11,7 +11,7 @@ class Soldier:
         return self.strength
         
     def receiveDamage(self, damage):
-        self.health -= damage
+        self.health = self.health - damage
         
 # Viking
 
@@ -28,7 +28,7 @@ class Viking (Soldier):
         return self.strength
     
     def receiveDamage(self, damage):
-        self.health -= damage
+        self.health = self.health -damage
         
         if self.health > 0:
             return self.name + ' has received ' + str(damage) + ' points of damage'
@@ -63,6 +63,7 @@ class Saxon (Soldier):
 
 # War
 
+import random
 
 class War:
     def __init__(self, vikingArmy, saxonArmy):
@@ -74,8 +75,41 @@ class War:
         
     def addSaxon(self, viking):
         self.saxonArmy.append(viking)
+         
+        viking = self.vikingArmy[random.radint(0, len(self.vikingArmy) -1)]
+      
+        damageSaxon = saxon.receiveDamage(viking.strength)
+
+        if saxon.health <1 : self.saxonArmy.pop(saxon)
+
+        return damageSaxon
         
-    def vikingAttack(self):
-        damage = viking.damage
         
-        
+        if viking.health < 1: self.vikingArmy.remove(viking)
+    def vikingAttack(self):                 
+
+        saxon = self.saxonArmy[random.randint(0, len(self.saxonArmy) -1)]
+        viking = self.vikingArmy[random.randint(0, len(self.vikingArmy) -1)]
+
+        damageSaxon = saxon.receiveDamage(viking.strength)
+        if saxon.health <1 : self.saxonArmy.pop(damageSaxon)
+        return damageSaxon
+
+    def saxonAttack(self):
+
+        saxon = self.saxonArmy[random.randint(0, len(self.saxonArmy) -1)]
+        viking = self.vikingArmy[random.randint(0, len(self.vikingArmy) -1)]
+
+        damageViking = viking.receiveDamage(saxon.strength)
+        if viking.health <1: self.vikingArmy.pop(damageViking)
+        return damageViking
+    
+    def showStatus(self):
+
+        if len(self.saxonArmy) < 1: 
+            return 'Vikings have won the war of the century!'
+
+        elif len(self.vikingArmy) < 1:
+            return 'Saxons have fought for their lives and survive another day...'
+
+        else: return 'Vikings and Saxons are still in the thick of battle.'
